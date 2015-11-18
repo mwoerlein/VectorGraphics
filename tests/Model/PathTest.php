19ff3e020d9b6f9f8db0ece09a0aeb4f5f250412
Marc Wörlein <hassandor@gmx.de>
2015-11-18 15:20:23 +0100

    VG-0: add new test to trigger travis

--- tests/Model/PathTest.php
+++ tests/Model/PathTest.php
@@ -17,4 +17,16 @@ class PathTest extends TestCase
         $this->assertEquals(1.2, $elements[0]->getDestX());
         $this->assertEquals(3.4, $elements[0]->getDestY());
     }
+
+    public function testLineTo()
+    {
+        $path = new Path();
+        $this->assertEmpty($path->getElements());
+        $path->moveTo(2.3, 4.5);
+        $elements = $path->getElements();
+        $this->assertCount(1, $elements);
+        $this->assertInstanceOf(Path\LineTo::class, $elements[0]);
+        $this->assertEquals(2.3, $elements[0]->getDestX());
+        $this->assertEquals(4.5, $elements[0]->getDestY());
+    }
 }
