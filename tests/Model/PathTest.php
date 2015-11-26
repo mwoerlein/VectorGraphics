3172755cffdff5d9c345521a2eec5c41b25ef4e0
Marc Wörlein <hassandor@gmx.de>
2015-11-26 10:54:58 +0100

    VG-12: add missing visibility test for paths and fix phpunit version to 4.1.* because of IDE integration

--- tests/Model/PathTest.php
+++ tests/Model/PathTest.php
@@ -1,9 +1,17 @@
 <?php
-namespace VectorGraphics\Test\Model\Path;
+namespace VectorGraphics\Test\Model;
 
 use PHPUnit_Framework_TestCase as TestCase;
 use VectorGraphics\Model\Path;
 
+/**
+ * @covers VectorGraphics\Model\Path
+ * @covers VectorGraphics\Model\Path\Close
+ * @covers VectorGraphics\Model\Path\CurveTo
+ * @covers VectorGraphics\Model\Path\LineTo
+ * @covers VectorGraphics\Model\Path\MoveTo
+ * @covers VectorGraphics\Model\Path\PathElement
+ */
 class PathTest extends TestCase
 {
     /**
@@ -26,45 +34,45 @@ class PathTest extends TestCase
         $this->assertCount(3, $path->getElements());
         $path->close();
         $this->assertCount(4, $path->getElements());
-
+        
         $this->assertEquals($elements, $path->getElements());
     }
-
+    
     /**
      */
     public function testMoveTo()
     {
         $path = new Path();
         $path->moveTo(1.2, 3.4);
-
+        
         $elements = $path->getElements();
         $this->assertCount(1, $elements);
         $this->assertInstanceOf(Path\MoveTo::class, $elements[0]);
         $this->assertEquals(1.2, $elements[0]->getDestX());
         $this->assertEquals(3.4, $elements[0]->getDestY());
     }
-
+    
     /**
      */
     public function testLineTo()
     {
         $path = new Path();
         $path->lineTo(2.3, 4.5);
-
+        
         $elements = $path->getElements();
         $this->assertCount(1, $elements);
         $this->assertInstanceOf(Path\LineTo::class, $elements[0]);
         $this->assertEquals(2.3, $elements[0]->getDestX());
         $this->assertEquals(4.5, $elements[0]->getDestY());
     }
-
+    
     /**
      */
     public function testCurveTo()
     {
         $path = new Path();
         $path->curveTo(1.2, 2.3, 3.4, 4.5, 5.6, 6.7);
-
+        
         $elements = $path->getElements();
         $this->assertCount(1, $elements);
         $this->assertInstanceOf(Path\CurveTo::class, $elements[0]);
@@ -77,23 +85,23 @@ class PathTest extends TestCase
         $this->assertEquals(5.6, $element->getDestX());
         $this->assertEquals(6.7, $element->getDestY());
     }
-
+    
     /**
      */
-    public function testCloseLastMooveTo()
+    public function testCloseLastMoveTo()
     {
         $path = new Path();
         $path->moveTo(1, 2);
         $path->moveTo(2, 3);
         $path->moveTo(3, 4);
         $path->close();
-
+        
         $elements = $path->getElements();
         $this->assertCount(4, $elements);
         $this->assertEquals(3, $elements[3]->getDestX());
         $this->assertEquals(4, $elements[3]->getDestY());
     }
-
+    
     /**
      */
     public function testFailCloseWithoutMoveTo()
@@ -101,5 +109,28 @@ class PathTest extends TestCase
         $path = new Path();
         $this->setExpectedException(\Exception::class);
         $path->close();
+    }
+    
+    /**
+     */
+    public function testVisibility()
+    {
+        $path = new Path();
+        $this->assertFalse($path->isVisible());
+        
+        $path = new Path();
+        $path->moveTo(1, 2);
+        $path->close();
+        $this->assertFalse($path->isVisible());
+        
+        $path = new Path();
+        $path->moveTo(1, 2);
+        $path->lineTo(2, 3);
+        $this->assertTrue($path->isVisible());
+        
+        $path = new Path();
+        $path->moveTo(1, 2);
+        $path->curveTo(2, 3, 3, 4, 4, 5);
+        $this->assertTrue($path->isVisible());
     }
 }
